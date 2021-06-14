f0771e2cd4c755c32a0aec4ca19786c3b287fb01
fernandofacione <facione.fernando@gmail.com>
2021-06-14 20:30:08 -0300

    commit

--- src/main/java/devcalc/Calc.java
+++ src/main/java/devcalc/Calc.java
@@ -1,8 +1,6 @@
-// 1-Pacote
+// 1 - Pacote
 package devcalc;
-
 // 2 - Bibliotecas
-
 import java.util.Scanner;
 
 // 3 - Classe
@@ -10,8 +8,10 @@ public class Calc {
     // 3.1 - Atributos
     static Scanner entrada = new Scanner(System.in); // entrada = input
 
-    // 3.2 - Métodos e funções
-    public static void main(String[] args) {
+    // 3.2 - Métodos e Funções
+    public static void main(String[] args){
+
+        // Exibe o menu da calculadora
         System.out.println(">>> CALCULADORA <<<");
         System.out.println("(1) Somar");
         System.out.println("(2) Subtrair");
@@ -19,7 +19,7 @@ public class Calc {
         System.out.println("(4) Dividir");
         System.out.print("Escolha o Calculo Desejado: ");
 
-        //Recebe a opção desejada
+        // Recebe a opção desejada
         String opcao = entrada.nextLine();
 
         // Pergunta os valores a serem usados no calculo
@@ -27,16 +27,16 @@ public class Calc {
         int num1 = entrada.nextInt();
         System.out.print("Entre o 2º numero: ");
         int num2 = entrada.nextInt();
-        System.out.println("O resultado é: ");
+        System.out.print("O resultado é: ");
 
         // Chama a função do calculo desejado
         switch (opcao){
             case "1":
-                System.out.print(somarDoisNumeros(num1, num2));
+                System.out.println(somarDoisNumeros(num1, num2));
                 break;
 
             default:
-                System.out.println("Opcao invalida");
+                System.out.println("Opção invalida");
                 break;
         }
 
@@ -44,8 +44,5 @@ public class Calc {
 
     public static int somarDoisNumeros(int num1, int num2){
         return num1 + num2;
-
     }
 }
-
-
