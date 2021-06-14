c009cffffe671b42e1e722ba7da6d058b9295d61
fernandofacione <facione.fernando@gmail.com>
2021-06-14 19:39:18 -0300

    commit

---
+++ src/main/java/devcalc/Calc.java
@@ -0,0 +1,27 @@
+// 1-Pacote
+package devcalc;
+// 2 - Bibliotecas
+
+
+// 3 - Classe
+public class Calc {
+    // 3.1 - Atributos
+
+    // 3.2 - Métodos e funções
+    public static void main(String[] args) {
+        System.out.println(">>> CALCULADORA <<<");
+        System.out.println("(1) Somar");
+        System.out.println("(2) Subtrair");
+        System.out.println("(1) Multiplicar");
+        System.out.println("(1) Dividir");
+        System.out.println("Escolha o Calculo Desejado");
+        System.out.println("Passou pelo metodo main");
+        somarDoisNumeros();
+    }
+
+    public static void somarDoisNumeros() {
+        System.out.println("Passou pelo método de somarDoisNumeros");
+
+
+    }
+}
