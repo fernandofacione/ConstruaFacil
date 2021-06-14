7f3ed9c4e61f0e6ec1b818e3a778bfdc326feb6e
fernandofacione <facione.fernando@gmail.com>
2021-06-14 20:05:33 -0300

    commit

--- src/main/java/devcalc/Calc.java
+++ src/main/java/devcalc/Calc.java
@@ -14,18 +14,36 @@ public class Calc {
         System.out.println(">>> CALCULADORA <<<");
         System.out.println("(1) Somar");
         System.out.println("(2) Subtrair");
-        System.out.println("(1) Multiplicar");
-        System.out.println("(1) Dividir");
+        System.out.println("(3) Multiplicar");
+        System.out.println("(4) Dividir");
         System.out.println("Escolha o Calculo Desejado");
 
         String opcao = entrada.nextLine();
 
-        System.out.println("Passou pelo método de somarDoisNumeros");
-        somarDoisNumeros();
+
+        System.out.println("Entre o 1º numero: ");
+        int num1 = entrada.nextInt();
+        System.out.println("Entre o 2º numero: ");
+        int num2 = entrada.nextInt();
+
+        // Chama a função do calculo desejado
+        switch (opcao){
+            case "1":
+                somarDoisNumeros();
+                break;
+
+            default:
+                System.out.println("Opcao invalida");
+                break;
+        }
+
+        System.out.println("Passou pelo método main");
 
     }
-    public static void somarDoisNumeros(){
 
+    public static void somarDoisNumeros(int num1, int num2){
+
+        System.out.println("Passou pelo método somarDoisNumeros");
     }
 
 
