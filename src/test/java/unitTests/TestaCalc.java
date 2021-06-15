67c933210a1360561824a50ee253376e68d2e769
fernandofacione <facione.fernando@gmail.com>
2021-06-14 22:18:46 -0300

    commit

--- src/test/java/unitTests/TestaCalc.java
+++ src/test/java/unitTests/TestaCalc.java
@@ -1,4 +1,30 @@
 package unitTests;
 
+import devcalc.Calc;
+import org.testng.annotations.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class TestaCalc {
+    @Test
+    public void testarSomarDoisNumeros(){
+
+        // 1 - Preparar - Configurar - Given
+        int num1 = 5;
+        int num2 = 7;
+        int resultadoEsperado = 12;
+
+        // 2 - Executar - When
+        int resultadoAtual = Calc.somarDoisNumeros(num1, num2);
+
+
+        // 3 - Validar - Then
+        System.out.println("O resultado Esperado = " + resultadoEsperado + "\n O resultado Atual foi " + resultadoAtual);
+        assertEquals(resultadoEsperado,resultadoAtual);
+
+
+
+    }
+
 }
