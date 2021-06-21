f5e33d7f7b5b405fd10464adda71388c9a8962c2
fernandofacione <facione.fernando@gmail.com>
2021-06-21 20:34:11 -0300

    Aula 21/06/2021

--- src/test/java/apiTests/Pet.java
+++ src/test/java/apiTests/Pet.java
@@ -2,31 +2,45 @@ package apiTests;
 
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static io.restassured.RestAssured.given;
 
 
 public class Pet {
 
-    @Test
-    public void incluirPet() {
 
-        // Padrão
-        // Given = Dado
-        // .When = Quando
-        // .Then = Então
+    // Padrão
+    // Given = Dado
+    // .When = Quando
+    // .Then = Então
 
-        given()
-                .contentType("application/json")
-                .log().all()
-        .when()
-                .post("https://petstore.swagger.io/v2/pet")
-
-        .then()
-                .log() .all()
-        ;
+    // Funções de Apoio
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
+    @Test
+    public void incluirPet() throws IOException { // Create - Post
+
+        String jsonBody = lerJson("src/test/resources/data/pet.json");
+
+        given()                                                     //Dado
+                .contentType("application/json")                    //Tipo do Conteúdo
+                // "text/xml" para web services sincronos - ex: Correios
+                // "application/json" para web services assincronos - ex: ifood
+                .log().all()                                        //Registrar tudo do envio
+                .body(jsonBody)
+        .when()
+                .post("https://petstore.swagger.io/v2/pet")      //Comando + endpoint
+        .then()                                                       //Então
+                .log().all()                                          //Registrar tudo da volta
+                .statusCode(200);                                      //Código do Estado
 
 
+
+    }
 
 }
