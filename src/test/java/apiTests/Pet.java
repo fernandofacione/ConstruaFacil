e436cb87a1af263797f48abe2de92027a2dbe97e
fernandofacione <facione.fernando@gmail.com>
2021-06-23 19:55:23 -0300

    Aula 21/06/2021

--- src/test/java/apiTests/Pet.java
+++ src/test/java/apiTests/Pet.java
@@ -1,15 +1,15 @@
 package apiTests;
 
-import com.sun.org.apache.xpath.internal.operations.String;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.*;
 
 
 public class Pet {
@@ -30,42 +30,44 @@ public class Pet {
 
         String jsonBody = lerJson("src/test/resources/data/pet.json");
 
-        given()                                                     //Dado
-                .contentType("application/json")                    //Tipo do Conteúdo
+        given()                                                 //Dado
+                .contentType("application/json")                //Tipo do conteúdo
                 // "text/xml" para web services sincronos - ex: Correios
-                // "application/json" para web services assincronos - ex: ifood
-                .log().all()                                        //Registrar tudo do envio
+                // "application/json" para web services assincronos - ex: iFood
+                .log().all()                                    //Registrar tudo do envio
                 .body(jsonBody)
-                .when()
-                .post("https://petstore.swagger.io/v2/pet")        //Comando + endpoint
-                .then()                                                         //Então
-                .log().all()                                            //Registrar tudo da volta
-                .statusCode(200)                                        //ValidarCódigo do Estado
-                .body("id", is(1110))                        //Valida o ID
-                .body("name", is("Pateta"))                  //Valida o nome do animal
-                .body("category.name", is("dog"))           //Valida a categoria do animal
-                .body("tags.name", contains("vermifugado ok"))    //Valida se contem a palavra chave
+                .when()                                                 //Quando
+                .post("https://petstore.swagger.io/v2/pet")     //Comando + endpoint
+                .then()                                                 // Então
+                .log().all()                                    // Registrar tudo da volta
+                .statusCode(200)                                // Valida o Código do Estado Nativo
+                // .body("code", is(200))               // Valida o Código de Estado no Json
+                .body("id", is(1110))        // Valida o id do animal
+                .body("name", is("Pateta"))            // Valida o nome do animal
+                .body("category.name", is("dog"))       // Valida a categoria do animal
+                //.body("tags.name", not(contains("não vermifugado")))  // Valida se contem a palavra chava
+                //.body("tags.id[0]", is(3))
+                //.body("tags.name[0]", stringContainsInOrder("primeira semana"))
+                //.body("tags.id[1]", is(4))
+                //.body("tags.name[1]", stringContainsInOrder("segunda semana"))
         ;
-
     }
 
-        @Test
-        public void incluirPet(){
+    @Test
+    public void consultarPet(){
+        String petId = "1101"; // Id do animal
 
-                String petID = "1110";
-
-                given()
-                        .contentType("application/json")
-                        .log().all()
-
+        given()
+                .contentType("application/json")
+                .log().all()
                 .when()
-                        .get("https://petstore.swagger.io/v2/pet" + petID)
-
+                .get("https://petstore.swagger.io/v2/pet/" + petId) // Get == Consultar
                 .then()
-                        .
-        }
-
-
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Atena"))
+                .body("status", is("available"))
+        ;
     }
 
 }
