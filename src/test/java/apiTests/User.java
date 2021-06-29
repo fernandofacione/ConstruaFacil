2418d8607adff7b030462266c8be40b56bb6d190
fernandofacione <facione.fernando@gmail.com>
2021-06-28 22:23:32 -0300

    Aula 28/06/2021

--- src/test/java/apiTests/User.java
+++ src/test/java/apiTests/User.java
@@ -12,6 +12,9 @@ import static org.hamcrest.Matchers.is;
 public class User {
        String uri = "https://petstore.swagger.io/v2/user";
        int userId = 2110;
+       String username = "darthvader";
+       String password = "123456";
+       String token = "";
 
         // Padrão
         // Given = Dado
@@ -44,5 +47,53 @@ public class User {
 
     }
 
+        @Test
+        public void consultarUsuario() {
 
+            given()
+                    .contentType("application/json")
+                    .log().all()
+            .when()
+                    .get(uri + "/" + username)
+            .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("id", is(userId))
+                    .body("username", is(username))
+                    .body("firstName", is("Darth"))
+                    .body("lastName", is("Vader"))
+                    .body("email", is("darthvader@test.com"))
+                    .body("password", is("123456"))
+                    .body("phone", is("55998895090"))
+                    .body("userStatus", is(0))
+
+            ;
+
+        }
+
+        @Test
+        public void login() {
+
+        String mensagem =
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + "/login?username="+ username + "&password=" + password)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+        .extract()
+                .path("message")
+        ;
+
+            System.out.println("A mesangem é :" + mensagem);
+            token = mensagem.substring(23);
+            System.out.println("O token é :" + token);
+
+
+
+        }
 }
