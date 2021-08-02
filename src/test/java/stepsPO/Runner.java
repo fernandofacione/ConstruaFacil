3a8b16286e57c54927a499a159a559aebb67ac9b
Fernando Facione <85024081+fernandofacione@users.noreply.github.com>
2021-08-02 20:43:08 -0300

    01-08-2021

---
+++ src/test/java/stepsPO/Runner.java
@@ -0,0 +1,24 @@
+package stepsPO;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.Test;
+
+@Test
+@CucumberOptions(
+        features = {"src/test/resources/featuresPO"},           // Onde estão os cenários em Gherkin
+        glue     = { "stepsPO" }  ,                             // Onde estão as definições de passos
+        dryRun   = false,                                       // Exibição de log
+        monochrome = true,                                      // Detalhes do log
+        plugin = {
+                    "pretty",                                   // Formatação visual do Cucumber
+                    "html:target/reports/extentreports",        // Saida HTML do Relatório Simples
+                    "json:target/reports/extentreports.json"    // Saida Json com os dados
+
+
+        }
+
+)
+public class Runner extends AbstractTestNGCucumberTests { // Equivale ao @RunWith do JUnit
+    // Configuração de modelo de relatório ou de outra característica
+}
